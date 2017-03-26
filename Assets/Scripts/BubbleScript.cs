230d5691c80017c070997fd618f022f62332b522
David Kong <xlimitlessskiesx@gmail.com>
2017-03-26 13:54:15 -0400

    Bubbles bounce like bouncy bubbles do.

--- Assets/Scripts/BubbleScript.cs
+++ Assets/Scripts/BubbleScript.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class BubbleScript : MonoBehaviour
 {
 	[SerializeField]
-	public float ProjectileSpeed = 2.0f;
+	public float ProjectileSpeed = 5.0f;
 
 	public Point GridPosition { get; private set; }
 
@@ -16,8 +16,19 @@ public class BubbleScript : MonoBehaviour
 
 	void OnTriggerEnter2D(Collider2D coll) {
 		if (coll.tag.Equals("BubbleWall")) {
-			this.GetComponent<Rigidbody2D> ().velocity = this.GetComponent<Rigidbody2D> ().velocity * -1f;
-			print ("collision");
+//			float worldStartX = LevelManager.Instance.worldStart.x;
+//			float tileSize = LevelManager.Instance.TileSize;
+//			print ("collision " +  transform.position);
+//			Vector2 directionalChange = new Vector2 (-1, -1);
+//			if (transform.position.x == worldStartX - tileSize / 2
+//				|| transform.position.x == worldStartX - tileSize / 2 + (tileSize * LevelManager.Instance.MapX)) {
+//				print ("Redirecting along x axis");
+//				directionalChange = new Vector2 (-1, 1);
+//			} else {
+//				directionalChange = new Vector2 (1, -1);
+//			}
+//
+//			this.GetComponent<Rigidbody2D> ().velocity = Vector2.Scale (this.GetComponent<Rigidbody2D> ().velocity, directionalChange);
 		}
 	}
 
@@ -25,7 +36,14 @@ public class BubbleScript : MonoBehaviour
 		this.GridPosition = gridPosition;
 		this.transform.position = worldPosition;
 		transform.SetParent (parent);
-		this.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, 1f) * ProjectileSpeed;
+		this.GetComponent<Rigidbody2D>().velocity = generateRandomUnitVector() * ProjectileSpeed;
+	}
+
+	private Vector2 generateRandomUnitVector() {
+		float angle = Random.Range (0, 2 * Mathf.PI);
+		// Calculate the x and y component using good ol' math.
+
+		return new Vector2 (Mathf.Cos (angle), Mathf.Sin (angle));
 	}
 
 }
