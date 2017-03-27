60ad8b24b65e80f7114f075f1096cc8c4c210278
Asghar Khan <asgharakhan94@gmail.com>
2017-03-26 22:42:05 -0400

    projectiles destory work in progress

--- Assets/Scripts/TowerProjectile.cs
+++ Assets/Scripts/TowerProjectile.cs
@@ -11,7 +11,7 @@ public class TowerProjectile : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
+        currentTile();
     }
 
     void OnTriggerEnter2D(Collider2D other) {
@@ -19,5 +19,28 @@ public class TowerProjectile : MonoBehaviour {
             Destroy(other.gameObject);
             Destroy(gameObject);
         }
+    }
+
+    void currentTile() {
+        Vector3 pos = transform.position;
+        LevelManager lm = LevelManager.Instance;
+
+        int tileSize = (int)(lm.TileSize * 100);
+
+        // Don't look at this disgusting mess.
+        // For some reason our main developer decided to use a float for tile size and
+        // 1 pixel is .01 in value. 
+        int x = ((int)System.Math.Abs((lm.worldStart.x + pos.x) * 100) / tileSize);
+        int y = ((int)System.Math.Abs((lm.worldStart.y - pos.y) * 100) / tileSize);
+
+        Point current = new Point(x, y);
+        Debug.Log("(" + lm.worldStart.x + "," + lm.worldStart.y + ")");
+        //Debug.Log("(" + pos.x + "," + pos.y + ")");
+        //Debug.Log("(" + current.X + "," + current.Y + ")");
+
+        TileScript tile = lm.Tiles[current]; 
+        if (tile.Type == "water") {
+            Destroy(gameObject);
+        } 
     }
 }
